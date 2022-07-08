9e63e958ae00d2723430f37ee48f8bbabc991182
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-07-08 16:04:06 -0300

    q2

---
+++ Q2-prolog-logica.pl
@@ -0,0 +1,15 @@
+% Programação lógica com Prolog
+% Autor: Aguiar  Data: 08.07.2022
+% Universidade Federal do Ceará - campus Crateús
+% 
+% Programa questão 02
+/* pais(Nome, area, populacao) */
+pais('Brasil', 9, 130).
+pais('China', 12, 1800).
+pais('EUA', 9, 230).
+pais('India', 3, 450).
+
+% Densidade demográfica de cada um dos países;
+dens(N,D) :- pais(N,A,P), D is P/A.
+
+% consulta: dens(N,D).
