fc93f73b5dd36db5b089636ef452a048168d794b
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-07-12 15:58:49 -0300

    .

---
+++ QExtra-operacoes.pl
@@ -0,0 +1,48 @@
+Last login: Tue Jul 12 15:43:31 on ttys000
+naxos:~ aguiar$ nano
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  GNU nano 2.0.6                New Buffer                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Page ^K Cut Text  ^C Cur Pos
+^X Exit      ^J Justify   ^W Where Is  ^V Next Page ^U UnCut Text^T To Spell
